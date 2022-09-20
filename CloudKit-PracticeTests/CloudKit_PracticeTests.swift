2aced105c1164330be79c6ada2fc72d22206382a
chun0917 <avnyhfgszaybj@gmail.com>
2022-09-20 16:02:20 +0800

    chun push

---
+++ CloudKit-PracticeTests/CloudKit_PracticeTests.swift
@@ -0,0 +1,33 @@
+//
+//  CloudKit_PracticeTests.swift
+//  CloudKit-PracticeTests
+//
+//  Created by 呂淳昇 on 2022/9/19.
+//
+
+import XCTest
+@testable import CloudKit_Practice
+
+class CloudKit_PracticeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
